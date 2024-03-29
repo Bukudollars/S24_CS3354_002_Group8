2c13890063d86735ce9198e04a12acf0ab77fdd8
James Wang <james.wang12345@gmail.com>
2024-03-29 14:14:14 -0500

    Integration Test
    wrote integration test for AppController

---
+++ src/test/java/com/Waterboi/API/AppControllerTest.java
@@ -0,0 +1,27 @@
+package com.Waterboi.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import com.waterboi.api.controller.AppController;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AppControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void registerShouldReturnRegisterPage() throws Exception {
+        this.mockMvc.perform(get("/register"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("register"));
+    }
+}
