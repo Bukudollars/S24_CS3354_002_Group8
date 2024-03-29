0b9acd93fe2a3e26cdb2e3767b1bc795e60843d9
James Wang <james.wang12345@gmail.com>
2024-03-28 20:24:53 -0500

    Setup Login

---
+++ API/src/main/java/com/Waterboi/API/model/Appuser.java
@@ -0,0 +1,24 @@
+package com.waterboi.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Appuser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    public Appuser(String username, String password) {
+        this.username = username.toLowerCase();
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
