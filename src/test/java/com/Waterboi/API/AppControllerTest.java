f2ffcc498764d08c2e84d6bba8964043e7fb06b9
James Wang <james.wang12345@gmail.com>
2024-03-29 14:19:40 -0500

    Removed extra imports

--- src/test/java/com/Waterboi/API/AppControllerTest.java
+++ src/test/java/com/Waterboi/API/AppControllerTest.java
@@ -3,11 +3,9 @@ package com.Waterboi.API;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import com.waterboi.api.controller.AppController;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
