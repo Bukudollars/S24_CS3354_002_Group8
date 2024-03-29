3d7859768d1c54dcb7e426ae90782979474d1fa0
James Wang <james.wang12345@gmail.com>
2024-03-29 14:50:03 -0500

    added login test
    added test to register a user then login

--- src/test/java/com/Waterboi/API/AppControllerTest.java
+++ src/test/java/com/Waterboi/API/AppControllerTest.java
@@ -1,12 +1,14 @@
 package com.Waterboi.API;
 
+import com.waterboi.api.model.Appuser;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -21,5 +23,30 @@ public class AppControllerTest {
         this.mockMvc.perform(get("/register"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("register"));
+    }
+
+    @Test
+    public void whenRegisterAppuser_theAppuserIsRegistered() throws Exception {
+
+        mockMvc.perform(post("/register")
+                .param("username", "newUser")
+                .param("password", "password")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+    }
+
+    @Test
+    public void whenAppuserRegsitered_AppuserCanLogin() throws Exception {
+        mockMvc.perform(post("/register")
+                .param("username", "newUser")
+                .param("password", "password")
+                .with(csrf()));
+        mockMvc.perform(post("/login")
+                .param("username", "newUser")
+                .param("password", "password")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/home"));
     }
 }
