ba4974788b22eea124cc0db9b366eedf5c749ca0
James Wang <james.wang12345@gmail.com>
2024-03-30 18:51:01 -0500

    Added Post water API Endpoint
    Added water post endpoint /api/post/new

--- waterboi/api/src/main/java/com/Waterboi/API/controller/WaterPostController.java
+++ waterboi/api/src/main/java/com/Waterboi/API/controller/WaterPostController.java
@@ -2,12 +2,14 @@ package com.Waterboi.API.controller;
 
 import com.Waterboi.API.entity.WaterPost;
 import com.Waterboi.API.repository.AppuserRepository;
+import com.Waterboi.API.repository.UnitOfMeasureRepository;
 import com.Waterboi.API.repository.WaterPostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/post")
@@ -17,10 +19,26 @@ public class WaterPostController {
     private AppuserRepository appuserRepository;
     @Autowired
     private WaterPostRepository waterPostRepository;
+    @Autowired
+    private UnitOfMeasureRepository unitOfMeasureRepository;
 
     @GetMapping("/all")
     public List<WaterPost> getAllPosts() {
         return waterPostRepository.findAll();
+    }
+
+    @PostMapping("/new")
+    public WaterPost newWaterPost(@RequestBody WaterPostDto waterPostDto) {
+        if (waterPostDto.quantity() < 0.0) {throw new IllegalArgumentException("Quantity cannot be negative.");}
+        if (!unitOfMeasureRepository.existsById(waterPostDto.unitOfMeasureId())) {
+            throw new NoSuchElementException("Unit of measure ID not found");
+        }
+
+        return waterPostRepository.save(new WaterPost(
+                1l,
+                waterPostDto.quantity(),
+                waterPostDto.unitOfMeasureId(),
+                LocalDateTime.now()));
     }
 
 }
