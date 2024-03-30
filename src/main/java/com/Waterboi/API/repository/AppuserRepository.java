cdd63b4bb421a1b408e70656083fba0b6fb11b96
James <1596698+Bukudollars@users.noreply.github.com>
2024-03-29 19:40:41 -0500

    Fixed file structure

--- src/main/java/com/Waterboi/API/repository/AppuserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.waterboi.api.repository;
-
-import com.waterboi.api.model.Appuser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppuserRepository extends JpaRepository<Appuser, Long> {
-    Appuser findByUsernameIgnoreCase(String username);
-}
