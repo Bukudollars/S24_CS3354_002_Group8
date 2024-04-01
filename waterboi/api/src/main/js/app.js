48fb35f5aabfedd4f1777d859b27974f71bf2ddf
James Wang <james.wang12345@gmail.com>
2024-03-31 20:19:48 -0500

    Front End all posts
    Front end to display all posts

---
+++ waterboi/api/src/main/js/app.js
@@ -0,0 +1,38 @@
+document.getElementById('helloWorld').innerText = 'Hello World';
+import React, { useState, useEffect } from 'react';
+function App() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/post/all')
+        .then(response => response.json())
+        .then(data => setData(data))
+        .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
+    return (
+        <div className="App">
+            <table>
+                <thead>
+                    <tr>
+                        <th>appuserId</th>
+                        <th>quantity</th>
+                        <th>unitOfMeasureId</th>
+                        <th>postTime</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(item => (
+                        <tr key={item.id}>
+                            <td>{item.appuserId}</td>
+                            <td>{item.quantity}</td>
+                            <td>{item.unitOfMeasureId}</td>
+                            <td>{item.postTime}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+export default App;
--- waterboi/api/webpack.config.js
+++ waterboi/api/webpack.config.js
@@ -1,10 +1,25 @@
 var path = require('path');
 
 module.exports = {
-    entry: './src/main/js/app.js',
+    entry: './src/main/js/index.js',
     output: {
         path: __dirname,
         filename: './src/main/resources/static/built/bundle.js'
     },
     mode: 'development',
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }
+            }
+        ]
+    }
 };
