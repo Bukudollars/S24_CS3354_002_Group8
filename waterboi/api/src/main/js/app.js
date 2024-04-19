ef89b8817a2844d1ff72039fc639ffa73e52f9dd
James Wang <james.wang12345@gmail.com>
2024-04-18 20:28:00 -0500

    added app mockup

--- waterboi/api/src/main/js/app.js
+++ waterboi/api/src/main/js/app.js
@@ -7,30 +7,33 @@ import HomeIcon from '@mui/icons-material/Home';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import SettingsIcon from '@mui/icons-material/Settings';
 import './app.css';
+import Dashboard from './dashboard/dashboard.js';
 function App() {
     return (
-        <Container>
-            <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <Box display="flex" justifyContent="center">
-                        <Typography variant="h1" component="h2" gutterBottom className="page-title">
-                            Water Tracker
-                        </Typography>
-                    </Box>
+        // <Container>
+        //     <Grid container spacing={3}>
+        //         <Grid item xs={12}>
+        //             <Box display="flex" justifyContent="center">
+        //                 <Typography variant="h1" component="h2" gutterBottom className="page-title">
+        //                     Water Tracker
+        //                 </Typography>
+        //             </Box>
                     
-                </Grid>
-                <Grid item xs={12}>
-                {ViewPosts()}
-                </Grid>
-                <Grid item xs={12}>
-                    <BottomNavigation className='bottom-nav'>
-                        <BottomNavigationAction label="Home" icon={<HomeIcon className='nav-icon' />} />
-                        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon className='nav-icon' />} />
-                        <BottomNavigationAction label="Settings" icon={<SettingsIcon className='nav-icon' />} />
-                    </BottomNavigation>
-                </Grid>
-            </Grid> 
-        </Container>
+        //         </Grid>
+        //         <Grid item xs={12}>
+        //         {ViewPosts()}
+        //         </Grid>
+        //         <Grid item xs={12}>
+        //             <BottomNavigation className='bottom-nav'>
+        //                 <BottomNavigationAction label="Home" icon={<HomeIcon className='nav-icon' />} />
+        //                 <BottomNavigationAction label="Favorites" icon={<FavoriteIcon className='nav-icon' />} />
+        //                 <BottomNavigationAction label="Settings" icon={<SettingsIcon className='nav-icon' />} />
+        //             </BottomNavigation>
+        //         </Grid>
+        //     </Grid> 
+        // </Container>
+        <Dashboard />
+
     );
 }
 
