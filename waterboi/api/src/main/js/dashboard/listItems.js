87199be4e4ce0abb0d80fa5f154a7d62c69745f1
James Wang <james.wang12345@gmail.com>
2024-04-19 08:38:20 -0500

    Set Goal
    Completed set goal

--- waterboi/api/src/main/js/dashboard/listItems.js
+++ waterboi/api/src/main/js/dashboard/listItems.js
@@ -6,6 +6,7 @@ import ListSubheader from '@mui/material/ListSubheader';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import PeopleIcon from '@mui/icons-material/People';
+import LocalDrinkIcon from '@mui/icons-material/LocalDrink';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import LayersIcon from '@mui/icons-material/Layers';
 import AssignmentIcon from '@mui/icons-material/Assignment';
@@ -24,7 +25,7 @@ export function MainListItems() {
     </ListItemButton>
     <ListItemButton onClick={() => setCurrentView('drinks')}>
       <ListItemIcon>
-        <ShoppingCartIcon />
+        <LocalDrinkIcon />
       </ListItemIcon>
       <ListItemText primary="Drinks" />
     </ListItemButton>
--- waterboi/api/src/main/js/profile/Profile.js
+++ waterboi/api/src/main/js/profile/Profile.js
@@ -7,20 +7,33 @@ export default function Profile() {
     fetch('/api/user/profile')
       .then(response => response.json())
       .then(data => setUser(data));
+      console.log(user);
   }, []);
 
   const handleChange = (event) => {
     setUser({ ...user, [event.target.name]: event.target.value });
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch('/api/user/profile/goal/day', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ goal: user.dailyGoal }),
+    });
+  };
+
   return (
     <Container sx={{maxWidth: 'sm'}}>
       <Typography variant="h4" align="center">User Profile</Typography>
-      <form noValidate autoComplete="off">
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
         <TextField
           name="dailyGoal"
           label="Daily Goal"
-          value={user.name}
+          value={user.dailyGoal}
           onChange={handleChange}
           fullWidth
           margin="normal"
@@ -43,7 +56,7 @@ export default function Profile() {
           rows={4}
           margin="normal"
         /> */}
-        <Button variant="contained" color="primary" fullWidth>
+        <Button variant="contained" color="primary" fullWidth type="submit">
           Save
         </Button>
       </form>
